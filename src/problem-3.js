4d0ca3ce3822b1f68921631acbd7a52854bf5734
Tomasz Czechowski <tomasz.czechowski@rulefinancial.com>
2013-11-18 09:33:30 +0100

    Adding problem-3

---
+++ src/problem-3.js
@@ -0,0 +1,2 @@
+/*jshint evil: false, bitwise:false, strict: true, undef: true, white:false, onevar:false, nomen:false, browser:true, plusplus:false, white:false*/
+/*global define, Class, Backbone, _, debug, require, $ */
